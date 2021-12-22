988dafde4ecdfe7eecea7492b453a6d981c1072e
Rob Mitchelmore <rob@midworld.co.uk>
2021-12-22 14:13:46 +0000

    Code dump

---
+++ dlg.h
@@ -0,0 +1,1 @@
+#ifndef DLG_H#define DLG_Hint DoLoginDialog (StringPtr password);void InternalErrorDialog(Str255 errText, OSErr err);pascal Boolean	TwoItemFilter(DialogPtr dlog,EventRecord *event,short *itemHit);#endif
