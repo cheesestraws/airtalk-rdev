512df08be6d9aa108769d96c38b8f7a2229c1db7
Rob Mitchelmore <rob@midworld.co.uk>
2021-12-22 14:57:05 +0000

    add more comments

--- dlg.h
+++ dlg.h
@@ -1,6 +1,9 @@
 #ifndef DLG_H
 #define DLG_H
 
+/* dlg.{h,c} contain the functions that do user interface stuff; they manage the
+   login dialog and internal error signalling. */
+
 int DoLoginDialog (StringPtr password);
 void InternalErrorDialog(Str255 errText, OSErr err);
 pascal Boolean	TwoItemFilter(DialogPtr dlog,EventRecord *event,short *itemHit);
--- net.h
+++ net.h
@@ -1,5 +1,8 @@
 #include <MacTypes.h>
 
+/* net.{c,h} contain functions that send stuff out to the network or
+   receive stuff from it. */
+
 // sendConfiguration sends an ssid and a password to any airtalk that
 // happens to be listening.
 void sendConfiguration(Str255 ssid, Str255 pwd);
